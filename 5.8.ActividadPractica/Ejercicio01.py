f49cf6f7da00adf8535c838c39bfe1b8a6ea0183
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2025-02-14 19:28:51 -0300

    5.8 Actividad practica

--- 5.8.ActividadPractica/Ejercicio01.py
+++ 5.8.ActividadPractica/Ejercicio01.py
@@ -8,3 +8,20 @@
 # Información a tener en cuenta:
 # Se recuerda que los años bisiestos son múltiplos de 4, pero los múltiplos de 100 no lo son, aunque los múltiplos de 400 sí. 
 # Estos son algunos ejemplos de posibles respuestas: 2012 es bisiesto, 2010 no es bisiesto, 2000 es bisiesto, 1900 no es bisiesto.
+
+def solicitar_year(): 
+     while True:
+        year = int(input("Ingrese un año: "))
+        return year 
+
+def year_bisieto(year): 
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print( f"El año {year} es bisiesto.")  
+    else:
+        print( f"El año {year} no es bisiesto.")
+
+def main():
+    year = solicitar_year()
+    year_bisieto(year)
+
+main()
